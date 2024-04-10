9eb4a6ec680d1388f3f7d1041579fe0e440d834f
Alyssa Marcelo <alyssamarcelo17@gmail.com>
2024-04-11 02:54:48 +0800

    Add and update Lesson 12 Part 2 exercises

--- scripts/12-rock-paper-scissors.js
+++ scripts/12-rock-paper-scissors.js
@@ -20,6 +20,10 @@ let isAutoPlaying = false;
 
 let intervalId;
 
+// const autoPlay = () => {
+
+// }
+
 function autoPlay() {
   if (!isAutoPlaying) {
     intervalId = setInterval(() => {
@@ -33,6 +37,41 @@ function autoPlay() {
     isAutoPlaying = false;
   }
 }
+
+  document.querySelector('.js-rock-button').addEventListener('click', () => {
+      playGame('rock');
+    });
+
+  document.querySelector('.js-paper-button').addEventListener('click', () => {
+    playGame('paper');
+  });
+
+  document.querySelector('.js-scissors-button').addEventListener('click', () => {
+    playGame('scissors');
+  });
+
+  document.querySelector('.js-reset-button').addEventListener('click', () => {score.wins = 0;
+    score.losses = 0;
+    score.ties = 0;
+    localStorage.removeItem('score');
+    updateScoreElement();
+    autoPlay();
+  });
+
+  document.querySelector('.js-autoplay-button').addEventListener('click', () => {
+    autoPlay();
+  });
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's') {
+    playGame('scissors');
+  }
+} );
+
 
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
